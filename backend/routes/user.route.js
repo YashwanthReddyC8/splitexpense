bc791afb37de91557e0f4269721252d660c4fc8c
hnsk18 <23B81A6781@cvr.ac.in>
2026-05-29 23:58:57 +0530

    added routes for login,register,getprofile

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,1 +1,23 @@
-const express = require('express');
+const express = require("express");
+
+const router = express.Router();
+
+const userController = require("../controllers/user.controller");
+
+const auth = require("../middlewares/auth");
+
+
+// PUBLIC ROUTES
+router.post("/public/register", userController.register);
+
+router.post("/public/login", userController.login);
+
+
+// PRIVATE ROUTES
+router.get(
+    "/user/getprofile",
+    auth,
+    userController.getProfile
+);
+
+module.exports = router;
